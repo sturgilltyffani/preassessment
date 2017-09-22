5e5ea9bb1a0925a89f72e018619294585b6420b0
sturgilltyffani <31998266+sturgilltyffani@users.noreply.github.com>
2017-09-22 08:52:57 -0500

    PreWork Assessment Complete

---
+++ preassessment/script.js
@@ -0,0 +1,12 @@
+
+  $(document).ready(function(){
+    $('#btn1').click(function(){
+      alert('Hello');
+    });
+    $('#btn2').click(function(){
+      alert('How are you');
+    });
+    $('#btn3').click(function(){
+      alert('These buttons work!');
+    });
+  });
